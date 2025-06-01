337c340461a8b71fd4baac136ea8dc0eabd4113e
Rishika Snehi <snehirishika@gmail.com>
2025-06-01 13:17:16 +0530

    first commit

---
+++ react-portfolio/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+import { getImageUrl } from "../../utils";
+import styles from "./Navbar.module.css";
+
+export const Navbar = () => {
+    return (
+        <nav className={styles.navbar}>
+            <a className={styles.title} href="/">Portfolio</a>
+            <div className={styles.menu}>
+                <img className={styles.menuBtn} src={getImageUrl("nav/menuIcon.png")} alt="menu-button" />
+                <ul className={styles.menuItems}>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#experience">Experience</a></li>
+                    <li><a href="#projects">Projects</a></li>
+                    <li><a href="#contacts">Contact</a></li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
--- src/utils.js
+++
@@ -1,3 +0,0 @@
-export const getimageUrl = (path) =>{
-    return new URL(`/assets/${path}`,import.meta.url).href;
-};
