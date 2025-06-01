063045e1f239f482758aa18423a53926c665d9f9
Rishika Snehi <snehirishika@gmail.com>
2025-06-01 18:10:15 +0530

    formatted files

--- react-portfolio/src/components/Navbar/Navbar.jsx
+++ react-portfolio/src/components/Navbar/Navbar.jsx
@@ -13,14 +13,14 @@ export const Navbar = () => {
                     className={styles.menuBtn}
                     src={
                         menuOpen
-                        ? getImageUrl("nav/closeIcon.png")
-                        : getImageUrl("nav/menuIcon.png")
+                            ? getImageUrl("nav/closeIcon.png")
+                            : getImageUrl("nav/menuIcon.png")
                     }
                     alt="menu-button"
                     onClick={() => setMenuOpen(!menuOpen)}
                 />
                 <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
-                onClick={() => setMenuOpen(false)}>
+                    onClick={() => setMenuOpen(false)}>
                     <li><a href="#about">About</a></li>
                     <li><a href="#experience">Experience</a></li>
                     <li><a href="#projects">Projects</a></li>
