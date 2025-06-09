b6098383f1b9dbfed0f65b9aff9ffe668bbbb60f
Rishika Snehi <snehirishika@gmail.com>
2025-06-09 10:38:32 +0530

    Experience section added

--- react-portfolio/src/components/Experience/Experience.jsx
+++ react-portfolio/src/components/Experience/Experience.jsx
@@ -1,7 +1,47 @@
-import React from 'react'
+import React from 'react';
+import skills from "../../data/skills.json";
+import history from "../../data/history.json";
+import { getImageUrl } from '../../utils';
+import styles from "./Experience.module.css"
 
 export const Experience = () => {
-  return (
-    <div>Experience</div>
+  return (<section id="experience">
+    <h2>Experience</h2>
+    <div>
+      <div>{
+        skills.map((skill, id) => {
+          return (
+            <div key={id}>
+              <div>
+                <img src={getImageUrl(skill.imageSrc)} alt={skill.title} />
+              </div>
+              <p>{skill.title}</p>
+            </div>
+          );
+        })}
+      </div>
+      <ul>
+        {
+          history.map((historyItem, id) => {
+            return (
+              <li key={id}>
+                <img src={getImageUrl(historyItem.imageSrc)} alt={`${historyItem.organisation} Logo`}
+                />
+                <div>
+                  <h3>{`${historyItem.role}, ${historyItem.organisation}`}</h3>
+                  <p>{`${historyItem.startDate} - ${historyItem.endDate}`}</p>
+                  <ul>
+                    {historyItem.experiences.map((experience, id) => {
+                    return <li key={id}>{experience}</li>;
+                  })}
+                  </ul>
+                </div>
+              </li>
+            );
+          })
+        }
+      </ul>
+    </div>
+  </section>
   )
 }
