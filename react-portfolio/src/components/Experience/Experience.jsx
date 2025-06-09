02086a7cf81d193e44adbfbf695a75bcd3879c22
Rishika Snehi <snehirishika@gmail.com>
2025-06-09 19:25:52 +0530

    Experience section completed

--- react-portfolio/src/components/Experience/Experience.jsx
+++ react-portfolio/src/components/Experience/Experience.jsx
@@ -1,18 +1,19 @@
-import React from 'react';
+import React from "react";
 import skills from "../../data/skills.json";
 import history from "../../data/history.json";
 import { getImageUrl } from '../../utils';
 import styles from "./Experience.module.css"
 
 export const Experience = () => {
-  return (<section id="experience">
-    <h2>Experience</h2>
-    <div>
-      <div>{
+  return (
+  <section className={styles.container} id="experience">
+    <h2 className={styles.title}> Experience</h2>
+    <div className={styles.content}>
+      <div className={styles.skills}>{
         skills.map((skill, id) => {
           return (
-            <div key={id}>
-              <div>
+            <div key={id} className={styles.skill}>
+              <div className={styles.skillImageContainer}>
                 <img src={getImageUrl(skill.imageSrc)} alt={skill.title} />
               </div>
               <p>{skill.title}</p>
@@ -20,14 +21,14 @@ export const Experience = () => {
           );
         })}
       </div>
-      <ul>
+      <ul className={styles.history}>
         {
           history.map((historyItem, id) => {
             return (
-              <li key={id}>
+              <li key={id} className={styles.historyItem}>
                 <img src={getImageUrl(historyItem.imageSrc)} alt={`${historyItem.organisation} Logo`}
                 />
-                <div>
+                <div className={styles.historyItemDetails}>
                   <h3>{`${historyItem.role}, ${historyItem.organisation}`}</h3>
                   <p>{`${historyItem.startDate} - ${historyItem.endDate}`}</p>
                   <ul>
