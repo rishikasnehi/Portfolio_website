d3ad7042fb0dd662eae733583a3ff0ddbd80f6a8
Rishika Snehi <snehirishika@gmail.com>
2025-06-11 19:32:34 +0530

    styling of project section completed and resolved the overflow issue

--- react-portfolio/src/components/Projects/ProjectCard.jsx
+++ react-portfolio/src/components/Projects/ProjectCard.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import { getImageUrl } from '../../utils';
+import styles from "./ProjectCard.module.css";
 
 export const ProjectCard = ({ project: {title, imageSrc, description, skills, demo, source} }) => {
     return (
-    <div>
-        <img src={getImageUrl(imageSrc)} alt={`Image of ${title}`} />
-        <h3>{title}</h3>
-        <p>{description}</p>
-        <ul>
+    <div className={styles.container}>
+        <img src={getImageUrl(imageSrc)} alt={`Image of ${title}`} className={styles.image}/>
+        <h3 className={styles.title}>{title}</h3>
+        <p className={styles.description}>{description}</p>
+        <ul className={styles.skills}>
             {skills.map((skill, id) => {
-                <li key={id}>{skill}</li>
+                return (
+                <li key={id} className={styles.skill}>{skill}</li>
+                );
             })}
         </ul>
-        <div>
-            <a href={demo}>Demo</a>
-            <a href={source}>source</a>
+        <div className={styles.links}>
+            <a href={demo} className={styles.link}>Demo</a>
+            <a href={source} className={styles.link}>source</a>
         </div>
     </div>
     );
--- react-portfolio/src/components/Projects/Projects.jsx
+++ react-portfolio/src/components/Projects/Projects.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
-import { getImageUrl } from '../../utils'
-import styles from "./Projects.module.css"
-import projects from "../../data/projects.json"
+import React from 'react';
+import styles from "./Projects.module.css";
+import projects from "../../data/projects.json";
 import { ProjectCard } from "./ProjectCard";
 
 export const Projects = () => {
   return(
-    <section>Projects
-      <h2>Projects</h2>
-      <div>
+    <section className={styles.container}>
+      <h2 className={styles.title}>Projects</h2>
+      <div className={styles.projects}>
         {projects.map((project, id) => (
              <ProjectCard key={id} project = {project}/>
           ))}
