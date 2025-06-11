dfc8b3fe7f1ba4e4675efde239a71f8e4e8accf8
Rishika Snehi <snehirishika@gmail.com>
2025-06-11 19:15:13 +0530

    ProjectCard.jsx and ProjectCard.module.css added

--- react-portfolio/src/components/Projects/Projects.jsx
+++ react-portfolio/src/components/Projects/Projects.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
 import { getImageUrl } from '../../utils'
 import styles from "./Projects.module.css"
+import projects from "../../data/projects.json"
+import { ProjectCard } from "./ProjectCard";
 
-export const Project = () => {
-  return; 
+export const Projects = () => {
+  return(
+    <section>Projects
+      <h2>Projects</h2>
+      <div>
+        {projects.map((project, id) => (
+             <ProjectCard key={id} project = {project}/>
+          ))}
+      </div>
+    </section>
+  ); 
 }
